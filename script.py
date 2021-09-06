f306b82a50e1b327caf7025577bc1ecbc7c0576d
tttalhaa <78103636+tttalhaa@users.noreply.github.com>
2021-09-06 15:43:52 +0200

    Create script.py

---
+++ script.py
@@ -0,0 +1,37 @@
+class Script(object):
+
+    START_MSG = """<b>✋Merhaba {},
+Ben birçok yeteneğe sahip gelişmiş bir filtre botuyum!
+Filtreleme kapasitem için pratik bir sınır yok:)
+görmek icin <i>/help</i> komutlar ve daha fazla ayrıntı için.</b>
+"""
+
+
+    HELP_MSG = """
+<i>Beni grubunuza yönetici olarak ekleyin ve filtrelemeye başlayın :)</i>
+<b>Temel Komutlar;</b>
+/start - Calisip calismadığımı kontrol et!
+/help - Komut yardımı
+/about - Benimle ilgili bir şey!
+<b>Filter Komutlar;</b>
+<code>/ad yanıtı ekle</code>  - Ad için filtre ekle
+<code>/del name</code>  -  Delete filter
+<code>/dell</code>  -  Tüm filtreleri sil (Yalnızca Grup Sahibi!)
+<code>/viewfilters</code>  -  Sohbetteki tüm filtreleri listele
+<b>Bağlantı Komutları;</b>
+<code>/grup kimliğine bağlan</code>  -  Grubunuzu PM'ime bağlayın. Ayrıca basitçe kullanabilirsiniz,
+<code>/connect</code> in groups.
+<code>/bağlantılar</code>  -  Bağlantılarınızı yönetin.
+<b>Ekstralar;</b>
+/status  -  Botunuzun mevcut durumunu gösterir (Yalnızca Yetkilendirme Kullanıcısı)
+/id  - Kimlik bilgilerini gösterir
+<code>/bilgi kullanıcı kimliği</code> - Kullanıcı Bilgilerini gösterir. Ayrıntılar için bazı iletilere yanıt olarak <code>/info</code> kullanın!
+<b>© @LuciFiltzerBot</b>
+"""
+
+
+    ABOUT_MSG = """<b>🤖 BOT 🤖: Luci Filter Bot</b>
+<b⚜️yaratıcı⚜️ :</b> @Lucifermiyiz
+<b>🧷 Dilim :</b> <code>Python3</code>
+<b> 📢Bot Güncellemeleri :</b> <a href='https://t.me/LuciOyunBot_Duyuru'> Luci Oyun Bot Duyuru </a> 
+"""
